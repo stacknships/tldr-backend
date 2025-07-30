ebfafa63dfebe256cbbfcad7871a7352e39deff1
stacknships <faraujorg@gmail.com>
2025-07-30 14:00:57 +0100

    Create route.ts

---
+++ src/app/api/ping/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(req: NextRequest) {
+  return NextResponse.json({ pong: true }, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
+export async function OPTIONS() {
+  return NextResponse.json({}, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
